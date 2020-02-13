954d37dfdf6928b0497768f621b6f22a0c974845
Danijela <danijela@tcom.rs>
2020-02-13 16:55:24 +0100

    First commit, app setup

---
+++ src/components/App/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'Tiny Dancer',
+          artist: 'Tiny Dancer',
+          album: 'The White Raven',
+          id: 1
+        },
+        {
+          name: 'Tiny Dancer',
+          artist: 'Tiny Dancer',
+          album: 'The White Raven',
+          id: 2
+        }
+      ],
+      playlistName: 'My Playlist',
+      playlistTracks: [
+        {
+          name: 'So Emotional',
+          artist: 'Whitney Houston',
+          album: 'Whitney Houston Classics',
+          id: 1
+        },
+        {
+          name: 'So Emotional - 1',
+          artist: 'Whitney Houston',
+          album: 'Whitney Houston Classics',
+          id: 2
+        },
+        {
+          name: 'So Emotional - 2',
+          artist: 'Whitney Houston',
+          album: 'Whitney Houston Classics',
+          id: 3
+        }
+      ]
+    }
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+
+    if (tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks})
+  }
+
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+
+    tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({playlistTracks: tracks});
+
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+            <Playlist playlistName={this.state.playlistName} 
+            playlistTracks={this.state.playlistTracks} 
+            onRemove={this.removeTrack}/>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+}
+
+export default App;
