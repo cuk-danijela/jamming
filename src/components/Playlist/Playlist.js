390515629027dd1b45e411c8cdeaa68fcbee65f8
Danijela <danijela@tcom.rs>
2020-02-14 12:06:30 +0100

    Change the Name of a Playlist

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -4,12 +4,23 @@ import TrackList from '../TrackList/TrackList';
 
 
 class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
+    }
+
     render() {
         return (
             <>
                 <div className="Playlist">
-                    <input defaultValue={'New Playlist'} />
-                    <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
+                    <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
+                    <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
                     <button className="Playlist-save">SAVE TO SPOTIFY</button>
                 </div>
 
